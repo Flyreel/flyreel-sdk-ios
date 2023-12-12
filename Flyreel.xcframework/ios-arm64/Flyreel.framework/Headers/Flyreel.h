8739dbb128db28f65cd2c89f717e446732ec7434
Szymon Wojcik <szymon.wojcik@lexisnexisrisk.com>
2023-12-12 15:09:44 +0100

    A test version of binary added

---
+++ Flyreel.xcframework/ios-arm64/Flyreel.framework/Headers/Flyreel.h
@@ -0,0 +1,18 @@
+//
+//  Flyreel.h
+//  Flyreel
+//
+//  Created by leeric01 on 3/20/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Flyreel.
+FOUNDATION_EXPORT double FlyreelVersionNumber;
+
+//! Project version string for Flyreel.
+FOUNDATION_EXPORT const unsigned char FlyreelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Flyreel/PublicHeader.h>
+
+
